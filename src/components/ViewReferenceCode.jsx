f1c21f13bcb3898b7f17bce2c645586a3d26add9
Oluwadare Oluwatobiloba <tobidechamp15@gmail.com>
2025-01-21 02:05:56 +0100

    dev changes

--- src/components/ViewReferenceCode.jsx
+++ src/components/ViewReferenceCode.jsx
@@ -4,13 +4,20 @@ import { Link } from "react-router-dom";
 const ViewReferenceCode = () => {
   const referenceCode = localStorage.getItem("referenceCode");
   return (
-    <div className="fixed top-0 left-0 w-full px-2 h-screen bg-white bg-opacity-0 flex items-center justify-center gap-1">
-      Your reference code is <strong>{referenceCode} </strong>. Please copy this
-      code as it is essential for getting your ticket. Click
-      <Link to="/manage-ticket" className="text-primary ">
-        here{" "}
-      </Link>{" "}
-      to proceed and get your ticket to get ticket
+    <div className="fixed top-0 left-0 w-full px-2 h-screen bg-white bg-opacity-0 flex items-center flex-col justify-center gap-1 text-center md:!px-[30%]">
+      <div>
+        <strong> Important:</strong> Your reference code is{" "}
+        <strong>{referenceCode} </strong>.
+      </div>
+      Tickets can only be generated once, so please copy and save this code or
+      download it to ensure you have it ready when needed. Click
+      <div>
+        <Link to="/manage-ticket" className="text-primary ">
+          {" "}
+          here{" "}
+        </Link>
+        to proceed and get your ticket.
+      </div>
     </div>
   );
 };
