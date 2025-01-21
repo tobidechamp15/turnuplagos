732a0e4e3f35dcb191358f6a009d9b67f1550207
Oluwadare Oluwatobiloba <tobidechamp15@gmail.com>
2025-01-21 01:11:13 +0100

    dev changes

--- src/components/ViewReferenceCode.jsx
+++ src/components/ViewReferenceCode.jsx
@@ -5,10 +5,6 @@ const ViewReferenceCode = () => {
   const referenceCode = localStorage.getItem("referenceCode");
   return (
     <div className="fixed top-0 left-0 w-full px-2 h-screen bg-white bg-opacity-0 flex items-center justify-center gap-1">
-      Your reference Code is <strong>{referenceCode} </strong>navigate{" "}
-      <Link to="/manage-ticket" className="text-primary ">
-        here{" "}
-      </Link>
       Your reference code is <strong>{referenceCode} </strong>. Please copy this
       code as it is essential for getting your ticket. Click
       <Link to="/manage-ticket" className="text-primary ">
